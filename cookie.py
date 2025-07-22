8fe2a18308e110e0f074541a84d95b7f6e18e929
Ashishgupta361 <ashishjhi361@gmail.com>
2025-07-22 21:29:33 +0530

    Added : code for Redirection and Timeout

--- cookie.py
+++ cookie.py
@@ -1,11 +1,27 @@
 import requests
-
+# https://httpbin.org/#/Cookies/get_cookies
+#https://github.com/Ashishgupta361/Python_Api_Testing
 # https://rahulshettyacademy.com/
 # visit-month
-# if wee want to sent cookie then it should be dictionary format
+# if we want to sent cookie then it should be dictionary format
 cookie = {'visit-month': 'February'}
-response = requests.get('https://rahulshettyacademy.com/', cookies=cookie)
+response = requests.get('http://rahulshettyacademy.com/', cookies=cookie)
+# 301 for redirection
+print(response.history)
 print(response.status_code)
+
+# Redirection
+response = requests.get('http://rahulshettyacademy.com/',allow_redirects=False, cookies=cookie)
+# 301 for redirection
+# output will be 301 because we have set redirection as false, it will not navigate to next page
+print(response.status_code)
+
+# Timeout will wait for given time before response
+response = requests.get('http://rahulshettyacademy.com/',allow_redirects=False, cookies=cookie,timeout=1)
+# 301 for redirection
+# output will be 301 because we have set redirection as false, it will not navigate to next page
+print(response.status_code)
+
 
 res = requests.get('https://httpbin.org/cookies',cookies={'visit-year': '2022'})
 print(res.text)
